1deee876696636c0d30a2c53a08b72e3a96bc67b
Alex <specmashalexeysh@gmail.com>
2022-09-04 15:29:42 +0300

    Header is done

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+let menuBtn = document.querySelector(".icon-menu");
+let menu = document.querySelector(".menu__body");
+
+menuBtn.addEventListener('click', function (){
+	menu.classList.toggle('active');
+	
+})
